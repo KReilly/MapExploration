13ef4513d9f4ba24908484091de297fcfe3f1b0d
KReilly <kevin@basejump.io>
2017-05-28 04:51:11 -0500

    Added floating banners. Maintain forward facing during rotations.

--- src/nav3D.cpp
+++ src/nav3D.cpp
@@ -25,6 +25,23 @@ void nav3D::setup(){
     stylizedModel3.setScale(-1, -1, 1);
     stylizedModel4.setScale(-1, -1, 1);
     
+    pin.addVertex(ofPoint( -1.125, 0, 0));
+    pin.addVertex(ofPoint( -1.125, 4, 0));
+    pin.addVertex(ofPoint( 1.125, 4, 0));
+    pin.addVertex(ofPoint( 1.125, 0, 0));
+    
+    pin.addTriangle(2, 1, 0);
+    pin.addTriangle(3, 2, 0);
+    
+    int U=1;
+    int V=1;
+    
+    pin.addTexCoord(ofPoint(0 * U, 0 * V));
+    pin.addTexCoord(ofPoint(0 * U, 1 * V));
+    pin.addTexCoord(ofPoint(1 * U, 1 * V));
+    pin.addTexCoord(ofPoint(1 * U, 0 * V));
+
+    
     ground.set(500, 500);
     ground.setPosition(0, 0, 0);
     ground.setResolution(10, 10);
@@ -44,15 +61,50 @@ void nav3D::draw(){
     stylizedModel4.enableColors();
     
     ofPushMatrix();
-    ofRotateY(90);
-    ofSetColor(250, 250, 250, 255);
-    ofScale(0.75, 0.75, 0.75);
-    
-    stylizedModel1.drawFaces();
-    stylizedModel2.drawFaces();
-    stylizedModel3.drawFaces();
-    stylizedModel4.drawFaces();
+        ofRotateY(90);
+        ofSetColor(250, 250, 250, 255);
+        ofScale(0.75, 0.75, 0.75);
+        
+        stylizedModel1.drawFaces();
+        stylizedModel2.drawFaces();
+        stylizedModel3.drawFaces();
+        stylizedModel4.drawFaces();
     ofPopMatrix();
+    
+    
+    ofVec3f rotAxis = cam.getXAxis();
+    
+    ofPushMatrix();
+        ofSetColor(0,255, 0);
+        ofTranslate(0, 3, 0);
+        ofRotate(cam.getRoll(), rotAxis.x, rotAxis.y, rotAxis.z );
+        ofRotateY(cam.getHeading());
+        pin.draw();
+    ofPopMatrix();
+    
+    ofPushMatrix();
+        ofSetColor(255,255, 0);
+        ofTranslate(6, 3, -12);
+        ofRotate(cam.getRoll(), rotAxis.x, rotAxis.y, rotAxis.z );
+        ofRotateY(cam.getHeading());
+        pin.draw();
+    ofPopMatrix();
+    
+    ofPushMatrix();
+        ofSetColor(255,0, 0);
+        ofTranslate(12, 12, 24);
+        ofRotate(cam.getRoll(), rotAxis.x, rotAxis.y, rotAxis.z );
+        ofRotateY(cam.getHeading());
+        pin.draw();
+    ofPopMatrix();
+    
+    ofPushMatrix();
+        ofSetColor(222, 224, 217);
+        ofRotateX(90);
+        ofTranslate(0, 0, 5);
+        ground.draw();
+    ofPopMatrix();
+
     
     cam.end();
 }
